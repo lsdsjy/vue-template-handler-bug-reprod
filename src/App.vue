75f186fd204f2538c8ad938c8c0db35f8ab4f37c
lsdsjy <lsdsjy@163.com>
2022-07-01 11:45:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app" @click="
+    $emit('update:a');
+  ">
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+console.log(HelloWorld)
+</script>
